ec142c87bb8315be9632861b6f9cd6671dbc8eab
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-07-24 23:33:49 +0100

    Add back button

---
+++ components/backButton.js
@@ -0,0 +1,8 @@
+export default ({handleClick}) => (
+    <div
+        className="center tc w-10 pa2 mt5 grow-large"
+        onClick={handleClick}
+    >
+        <i className="f1 dark-gray fa fa-2x fa-arrow-circle-o-left animate hover-gray"></i>
+    </div>
+);
