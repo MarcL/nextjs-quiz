b6b2841c0915678cd25cee3600144759d45582b8
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-07-25 21:58:10 +0100

    Align answer to the middle of the block

--- components/questionAnswer.js
+++ components/questionAnswer.js
@@ -6,7 +6,7 @@ export default ({answer, onAnswer, id}) => (
         <div className="dtc w3 f1">
             <i className="dark-gray fa fa-2x fa-check-circle-o"></i>
         </div>
-        <div className="dtc v-top pl3">
+        <div className="dtc v-mid pl3">
             <p>{answer}</p>
         </div>
     </div>
