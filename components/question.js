daa2c598d9196256f2a0af0fb3acaaf419ac1e33
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-07-27 13:18:23 +0100

    Tweak CSS for question

--- components/question.js
+++ components/question.js
@@ -1,7 +1,7 @@
 import QuestionAnswerContainer from './questionAnswerContainer';
 
 export default ({question, onAnswer, subtitle}) => (
-    <div className="question">
+    <div className="question w-75 center">
         <h3 className="f3 tc">{question.title}</h3>
         <h4 className="f4 tc light-silver fw4">{subtitle}</h4>
         <QuestionAnswerContainer
